2cdf828abaa84bcf3ebd28d08083e04606082daf
Gurpreet Kaur Mahun <gurpreetkaurmahun@Gurpreets-MacBook-Air.local>
2024-04-06 20:38:15 +0100

    Update second branch

--- Models/AuthModel.cs
+++
@@ -1,5 +0,0 @@
-public class AuthModel
- {
- public string Email { get; set; }
- public string Password { get; set; }
- }
--- Models/CreateRole.cs
+++
@@ -1,4 +0,0 @@
- public class CreateRoleRequest
-    {
-        public string RoleName { get; set; }
-    }
